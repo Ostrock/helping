09faa7aa9984ad9984ec5cf8f8f58bbcc57c03b3
Ostrock <guilherme@ostrock.com.br>
2021-03-18 16:00:46 -0300

    Solution returning the currect value of daily sotck variation price. Chenged the import to be an alias, the clening and join

--- scrap and clean data/stock_daily_price_variation.py
+++ scrap and clean data/stock_daily_price_variation.py
@@ -1,16 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 def parseVIX():
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     r = requests.get('https://br.investing.com/currencies/us-dollar-index', headers= headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = bs(r.text, 'html.parser')
     variation = soup.find("div", {"class": "top bold inlineblock"})
-    variation = variation.find_all('span')  
-    vix = float((variation[3].text).replace(',','.').replace('%','').replace('+',''))
-    
-    vix = (((vix)/100*-1)+1 )
+    variation = variation.find_all('span') 
+    vix = float('.'.join(variation[3].text[1:-1].split(',')))
 
-    return vix 
-
-print(parseVIX())
+    return vix
